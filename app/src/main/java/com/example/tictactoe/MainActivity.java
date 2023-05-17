50371a679c9856a947adf13cd11f7a037166352c
Shivansh Bhatnagar <bhatnagarshivansh19@gmail.com>
2023-05-18 01:43:50 +0530

    game created

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -58,18 +58,3 @@ import java.util.Arrays;
             }
         }
 
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-        }
-    }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-    }
-}
